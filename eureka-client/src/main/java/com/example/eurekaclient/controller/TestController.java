fc866bba0552becb9c2312aa606b9ccecf037022
Pavel <P.A.Pisarev@yandex.ru>
2022-12-06 17:03:47 +0600

    setup for multiple instances

--- eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.example.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${eureka.instance.instance-id}")
+    public String instanceId;
+
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("test");
+        return ResponseEntity.ok("test"+" instance-id:"+instanceId);
     }
 }
